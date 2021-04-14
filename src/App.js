561850448628544bf671ce35f0b467b0ce2c8721
CIPHERTron <pritish.samal918@gmail.com>
2021-04-14 23:29:20 +0530

    Setup Routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import "./App.css"
-import Header from "./components/shared/Header/Header"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+
 import Home from "./components/Home/Home"
+import Header from "./components/shared/Header/Header"
+import Checkout from "./components/Checkout/Checkout"
 
 function App() {
 	return (
-		<div className='app'>
-			<Header />
-			<Home />
-		</div>
+		<Router>
+			<div className='app'>
+				<Header />
+				<Switch>
+					<Route exact path='/'>
+						<Home />
+					</Route>
+					<Route exact path='/checkout'>
+						<Checkout />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
 	)
 }
 
