f7efd3035969e67a405b1d174a39a54ee21d2483
CIPHERTron <pritish.samal918@gmail.com>
2021-04-14 11:35:40 +0530

    Add Firebase Config

---
+++ src/config/firebase.js
@@ -0,0 +1,12 @@
+const apiKey = process.env.REACT_APP_API_KEY
+const messagingSenderId = process.env.REACT_APP_MESSAGING_SENDER_ID
+const appId = process.env.REACT_APP_APPID
+
+const firebaseConfig = {
+	apiKey: apiKey,
+	authDomain: "clone-2d395.firebaseapp.com",
+	projectId: "clone-2d395",
+	storageBucket: "clone-2d395.appspot.com",
+	messagingSenderId: messagingSenderId,
+	appId: appId,
+}
