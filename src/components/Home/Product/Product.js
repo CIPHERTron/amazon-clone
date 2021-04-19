1e3604e4aa161dda8f78f53ab1b77da64eaece90
CIPHERTron <pritish.samal918@gmail.com>
2021-04-19 22:33:21 +0530

    Dispatch ADD_TO_BASKET action on button click

--- src/components/Home/Product/Product.js
+++ src/components/Home/Product/Product.js
@@ -1,7 +1,23 @@
 import React from "react"
+import { useStateValue } from "../../../redux/StateProvider"
 import "./Product.css"
 
-function Product({ title, image, price, rating }) {
+function Product({ id, title, image, price, rating }) {
+	const [{ basket }, dispatch] = useStateValue() // destructuring basket from state
+
+	const addToBasket = () => {
+		// Dispatch ADD_TO_BASKET action(item into data layer)
+		dispatch({
+			type: "ADD_TO_BASKET",
+			item: {
+				id: id,
+				title: title,
+				image: image,
+				price: price,
+				rating: rating,
+			},
+		})
+	}
 	return (
 		<div className='product'>
 			<div className='product_info'>
@@ -21,7 +37,7 @@ function Product({ title, image, price, rating }) {
 
 			<img src={image} alt='' />
 
-			<button>Add To Basket</button>
+			<button onClick={addToBasket}>Add To Basket</button>
 		</div>
 	)
 }
