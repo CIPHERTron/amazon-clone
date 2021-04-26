2938f3c435903456727ab800d4ad0f81caab4b8c
CIPHERTron <pritish.samal918@gmail.com>
2021-04-26 22:49:03 +0530

    Setup server | Trigger Cloud Functions

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,6 @@
-const functions = require("firebase-functions");
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const functions = require("firebase-functions")
+const express = require("express")
+const cors = require("cors")
+const stripe = require("stripe")(
+	"sk_test_51Ik5dOSGm7zaQ1rAYCFVbGe2R7Q1PtaY0E822aQ747odBZwq5rTPZV048qKzdfHQaAQatnfvcye7GzdtgmzOjfFb00TqCWJRaM"
+)
