5e9cd13ea02bccf1d8ada22b1a0c1814eac1934d
CIPHERTron <pritish.samal918@gmail.com>
2021-04-27 22:17:56 +0530

    Express app init

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,10 @@
-const functions = require("firebase-functions")
-const express = require("express")
-const cors = require("cors")
+/* eslint-disable no-unused-vars */
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
 const stripe = require("stripe")(
 	"sk_test_51Ik5dOSGm7zaQ1rAYCFVbGe2R7Q1PtaY0E822aQ747odBZwq5rTPZV048qKzdfHQaAQatnfvcye7GzdtgmzOjfFb00TqCWJRaM"
-)
+);
+
+// initialize app
+const app = express();
