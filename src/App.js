ecbd9295f3ca81df80363ec1ad955f4010c82627
CIPHERTron <pritish.samal918@gmail.com>
2021-04-28 23:44:59 +0530

    Add orders route

--- src/App.js
+++ src/App.js
@@ -1,52 +1,56 @@
-import React, { useEffect } from "react"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import { auth } from "./config/firebase"
-import { useStateValue } from "./redux/StateProvider"
-import "./App.css"
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { auth } from "./config/firebase";
+import { useStateValue } from "./redux/StateProvider";
+import "./App.css";
 
 // Stripe imports
-import { loadStripe } from "@stripe/stripe-js"
-import { Elements } from "@stripe/react-stripe-js"
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 
 // Import components
-import Home from "./components/Home/Home"
-import Header from "./components/shared/Header/Header"
-import Footer from "./components/shared/Footer/Footer"
-import Checkout from "./components/Checkout/Checkout"
-import Payment from "./components/Checkout/Payment/Payment"
-import Login from "./components/auth/Login"
+import Home from "./components/Home/Home";
+import Header from "./components/shared/Header/Header";
+import Footer from "./components/shared/Footer/Footer";
+import Checkout from "./components/Checkout/Checkout";
+import Payment from "./components/Checkout/Payment/Payment";
+import Login from "./components/auth/Login";
+import Orders from "./components/Checkout/Orders/Orders";
 
 const promise = loadStripe(
 	"pk_test_51Ik5dOSGm7zaQ1rANEE8PfKwTlgazBbxNpNNdKGnCKrwryTGWRT7eZup0QdRa2fUc1VwopywLEB6UqOaNPAxfUQJ00nsVQqyf8"
-)
+);
 
 function App() {
 	// eslint-disable-next-line
-	const [{ basket }, dispatch] = useStateValue()
+	const [{ basket }, dispatch] = useStateValue();
 	useEffect(() => {
 		auth.onAuthStateChanged((authUser) => {
-			console.log("The user is ", authUser)
+			console.log("The user is ", authUser);
 
 			if (authUser) {
 				dispatch({
 					type: "SET_USER",
 					user: authUser,
-				})
+				});
 			} else {
 				dispatch({
 					type: "SET_USER",
 					user: null,
-				})
+				});
 			}
-		})
+		});
 		// eslint-disable-next-line
-	}, [])
+	}, []);
 	return (
 		<Router>
 			<div className='app'>
 				<Switch>
 					<Route exact path='/login'>
 						<Login />
+					</Route>
+					<Route exact path='/orders'>
+						<Orders />
 					</Route>
 					<Route exact path='/checkout'>
 						<Header />
@@ -68,7 +72,7 @@ function App() {
 				</Switch>
 			</div>
 		</Router>
-	)
+	);
 }
 
-export default App
+export default App;
