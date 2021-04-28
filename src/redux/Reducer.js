624c16ac6582a7b2d9722c9cb41408754a70cef3
CIPHERTron <pritish.samal918@gmail.com>
2021-04-28 21:06:40 +0530

    Add EMPTY_BASKET action

--- src/redux/Reducer.js
+++ src/redux/Reducer.js
@@ -1,10 +1,10 @@
 export const initialState = {
 	basket: [],
 	user: null,
-}
+};
 
 export const getBasketTotal = (basket) =>
-	basket?.reduce((amount, item) => item.price + amount, 0)
+	basket?.reduce((amount, item) => item.price + amount, 0);
 
 const reducer = (state, action) => {
 	switch (action.type) {
@@ -12,33 +12,38 @@ const reducer = (state, action) => {
 			return {
 				...state,
 				basket: [...state.basket, action.item],
-			}
+			};
 		case "REMOVE_FROM_BASKET":
 			const index = state.basket.findIndex(
 				(basketItem) => basketItem.id === action.id
-			)
-			let newBasket = [...state.basket]
+			);
+			let newBasket = [...state.basket];
 
 			if (index >= 0) {
-				newBasket.splice(index, 1)
+				newBasket.splice(index, 1);
 			} else {
 				console.warn(
 					`Cant remove product (id: ${action.id}) as its not in basket!`
-				)
+				);
 			}
 
 			return {
 				...state,
 				basket: newBasket,
-			}
+			};
 		case "SET_USER":
 			return {
 				...state,
 				user: action.user,
-			}
+			};
+		case "EMPTY_BASKET":
+			return {
+				...state,
+				basket: [],
+			};
 		default:
-			return state
+			return state;
 	}
-}
+};
 
-export default reducer
+export default reducer;
